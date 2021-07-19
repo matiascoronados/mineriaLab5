d833bd2b071164f022dd676c638dd7bb8508dc11
Matias <matias.coronado@usach.cl>
2021-07-19 19:08:57 -0400

    Codigo 80%

--- lab5.R
+++ lab5.R
@@ -10,6 +10,11 @@ require(RWeka)
 ####################################################################################
 ####################################################################################
 
+#OJO: Es importante estandarizar las variables.
+#"sí hay que estandarizarlos, de lo contrario, los predictores de mayor magnitud eclipsarán a los de menor magnitud."
+#https://www.cienciadedatos.net/documentos/34_maquinas_de_vector_soporte_support_vector_machines
+
+
 url.data.set <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/00329/messidor_features.arff'
 data.raw <- read.csv(url.data.set, header=FALSE, comment.char = "@")
 df <- data.frame(data.raw)
@@ -47,7 +52,6 @@ df.final$amfm <- factor(df.final$amfm)
 df.final<-df.final[!(df.final$q==0),]
 
 datos.05 <- df.final[,-c(4,5,6,7,8,10,11,12,13,14,15,16)]
-datos.05.todo <- df.final[,-c(4,5,6,7,8,10,11,12,13,14,15)]
 datos.06 <- df.final[,-c(3,5,6,7,8,9,11,12,13,14,15,16)]
 datos.07 <- df.final[,-c(3,4,6,7,8,9,10,12,13,14,15,16)]
 datos.08 <- df.final[,-c(3,4,5,7,8,9,10,11,13,14,15,16)]
@@ -56,8 +60,6 @@ datos.10 <- df.final[,-c(3,4,5,6,7,9,10,11,12,13,15,16)]
 df.final <- df.final
 
 class <- df.final['class']
-
-
 
 
 
@@ -100,19 +102,27 @@ print(ranking)
 
 ranking<-InfoGainAttributeEval(class ~ . , data = datos.05) 
 print(ranking)
-datos.aux1 <- subset(datos.05.todo, select = -q)
+datos.aux1 <- subset(datos.05, select = -q)
 datos.aux2 <- subset(datos.aux1, select = -dd)
-datos.final <- subset(datos.aux2, select = -dm)
+datos.aux3 <- subset(datos.aux2, select = -dm)
 
 #         ps       nma.a       nex.a        amfm
 # 0.004251408 0.078224967 0.035143457 0.001100858
-ranking<-InfoGainAttributeEval(class ~ . , data = datos.final)
+ranking<-InfoGainAttributeEval(class ~ . , data = datos.aux3)
 print(ranking)
 
 
+datos.aux3$nma.a <- scale(datos.aux3$nma.a)
+datos.aux3$nex.a <- scale(datos.aux3$nex.a)
+ps <- data.frame(datos.aux3$ps)
+amfm <- data.frame(datos.aux3$amfm)
+class <- data.frame(datos.aux3$class)
+aux.1 <- subset(datos.aux3, select = -class)
+aux.2 <- subset(aux.1, select = -ps)
+aux.3 <- subset(aux.2, select = -amfm)
+aux.4 <- scale(aux.3)
 
-
-
+datos.final <- datos.aux3
 
 ## 70% of the sample size
 smp_size <- floor(0.70 * nrow(datos.final))
@@ -123,22 +133,24 @@ train <- datos.final[train_ind, ]
 test <- datos.final[-train_ind, ]
 
 
-
-#CREAR MODELO SVM (1)
+##########################################################################################
+############################## MODELO SVM SIN CAMBIOS ####################################
+##########################################################################################
 #Se define la formula con las clases
 formula=class ~ .
 #Se pasan los parametros, y se instancia el modelo
-model <- svm(formula, data = train)
+model <- svm(formula = class ~ ., data = train)
 print(model)
 summary(model)
+plot(cmdscale(dist(train[,-5])), col = as.integer(train[,5]), pch = c("o","+")[1:150 %in% model$index + 1])
 
 
 
+############################################
 #PREDECIR UTIULIZANDO LOS DATOS DE PRUEBA + MODELO 
 # Se pasa el modelo junto al conjunto de caracteristicas
 pred <- predict(model,test)
 table(pred, test$class)
-
 #VER VALORES DE DECISION 
 # compute decision values and probablities:
 # Agregando probability = TRUE se puede obtener las probabilidades de pertenencia a cada una de las clases
@@ -146,10 +158,7 @@ table(pred, test$class)
 pred <- predict(model, x, decision.values = TRUE)
 attr(pred, "decision.values")[1:4,]
 #attr(pred, "probability")[1:4,]
-
-#GRAFICAR MODELO
-# visualize (classes by color, SV by crosses):
-plot(cmdscale(dist(train[,-6])), col = as.integer(iris[,5]), pch = c("o","+")[1:150 %in% model$index + 1])
+############################################
 
 #UTILIZACION DE TUNE CON KERNEL LINEAL
 # Esta nos permite ver cuales son los hiperparametros mas adecuados para obtener el mejor modelo dentro del proceso
@@ -157,7 +166,23 @@ plot(cmdscale(dist(train[,-6])), col = as.integer(iris[,5]), pch = c("o","+")[1:
 # Este nos permite utilizar un esquema de validacion cruzada con el parametro CROSS
 ### Para un kernel lineal se utiliza el ranges list(cost = 2^(-1:4))
 # Se tiene que buscar cuales son los valores mas adecuados en base al rango anterior
-obj <- tune(svm, Species~., data = iris, kernel = "linear",            ranges = list(cost = 2^(-1:4)),            tunecontrol = tune.control(sampling = "cross", cross = 2 ))
+
+
+# ranges: Valores de hiperparametros que se van a evaluar
+# cost: Hiperparametro de penalizacion, este norma el balance entre el bias y varianza del modelo
+# Este afecta en la eleccion de los vectores de soporte.
+# C = 0 -> divicion perfecta (0 erorres) [Puede traer errores de overfitting]
+
+# Es necesario utulizar las maquinas de vector de soporte cuando no se puede dividir linealmente.
+# Esto consiste en expandir las dimenciones del espacio original.
+
+
+
+##########################################################################################
+############################## MODELO SVM CON KERNEL LINEAL ##############################
+##########################################################################################
+set.seed(100)
+obj <- tune(svm, class~., data = datos.final, kernel = "linear",ranges = list(cost = 2^(-4:4)),tunecontrol = tune.control(sampling = "cross", cross = 2 ))
 summary(obj)
 #El summary entrega el mejor parametro
 #Mejor rendimiento
@@ -167,17 +192,52 @@ summary(obj)
 # Como es una funcion lineal, el unico hiperparametro que se esta moviendo es la funcion de costo
 plot(obj)
 summary(obj$best.model)
+model <- obj$best.model
+plot(cmdscale(dist(datos.final[,-5])), col = as.integer(datos.final[,5]), pch = c("o","+")[1:150 %in% model$index + 1])
 
-#DESEMPEÃO DEL MEJOR MODELO
-pred <- predict(obj$best.model, x)
-table(pred, Species)
+#DESEMPEO DEL MEJOR MODELO
+pred <- predict(obj$best.model, datos.final[,-5])
+conf.matrix <- table(pred, datos.final[,5])
+
+VP <- conf.matrix[1]
+FP <- conf.matrix[3]
+VN <- conf.matrix[4]
+FN <- conf.matrix[2]
+
+precicion = VP / (VP + FP)
+recall = VP / (VP + FN)
+calculoF1 <- 2*precicion*recall/(precicion + recall)
+
+conf.matrix
+precicion
+recall
+calculoF1
+
+
+
+##########################################################################################
+############################## MODELO SVM CON KERNEL RADIAL ##############################
+##########################################################################################
 
 #UTILIZACION DE TUNE CON KERNEL RADIAL
 # Aqui se tiene que incorporar los hiperparametros de gamma y costo
-obj <- tune(svm, Species~., data = iris, kernel = "radial", ranges = list(gamma = 2^(-2:4), cost = 2^(-1:4), tunecontrol = tune.control(sampling = "cross", cross = 2 )))
+obj <- tune(svm, class~., data = datos.final, kernel = "radial", ranges = list(gamma = 2^(-2:4), cost = 2^(-1:4), tunecontrol = tune.control(sampling = "cross", cross = 2 )))
 summary(obj)
-pred <- predict(obj$best.model, x)
-table(pred, Species)
+
+plot(obj)
+summary(obj$best.model)
+model <- obj$best.model
+plot(cmdscale(dist(datos.final[,-5])), col = as.integer(datos.final[,5]), pch = c("o","+")[1:150 %in% model$index + 1])
+
+
+
+
+
+
+
+
+
+
 
 #UTILIZACION DE TUNE CON KERNEL RADIAL (EXTENDIENDO LA BUSQUEDA DE LOS HIPERPARAMETROS)
 obj <- tune(svm, Species~., data = iris, kernel = "radial", ranges = list(gamma = 2^(-7:12), cost = 2^(-7:14) , tunecontrol = tune.control(sampling = "cross", cross = 2)))
